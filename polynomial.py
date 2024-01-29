fa2b05c52ad029018f3de6b3dba05615649a598b
victor <vpverm@bu.edu>
2024-01-28 22:50:23 -0500

    cs 506 exercise part c

--- polynomial.py
+++ polynomial.py
@@ -5,6 +5,9 @@ class X:
     def __repr__(self):
         return "X"
 
+    def evaluate(self, x_value):
+        return x_value
+
 
 class Int:
     def __init__(self, i):
@@ -12,6 +15,9 @@ class Int:
 
     def __repr__(self):
         return str(self.i)
+
+    def evaluate(self, x_value):
+        return self.i
 
 
 class Add:
@@ -22,6 +28,9 @@ class Add:
     def __repr__(self):
         return repr(self.p1) + " + " + repr(self.p2)
 
+    def evaluate(self, x_value):
+        return self.p1.evaluate(x_value) + self.p2.evaluate(x_value)
+
 
 class Sub:
     def __init__(self, p1, p2):
@@ -30,6 +39,9 @@ class Sub:
 
     def __repr__(self):
         return repr(self.p1) + " - " + repr(self.p2)
+
+    def evaluate(self, x_value):
+        return self.p1.evaluate(x_value) - self.p2.evaluate(x_value)
 
 
 class Mul:
@@ -46,6 +58,9 @@ class Mul:
             return repr(self.p1) + " * ( " + repr(self.p2) + " )"
         return "( " + repr(self.p1) + " * " + repr(self.p2) + " )"
 
+    def evaluate(self, x_value):
+        return self.p1.evaluate(x_value) * self.p2.evaluate(x_value)
+
 
 class Div:
     def __init__(self, p1, p2):
@@ -61,6 +76,17 @@ class Div:
             return repr(self.p1) + " / ( " + repr(self.p2) + " )"
         return "( " + repr(self.p1) + " / " + repr(self.p2) + " )"
 
+    def evaluate(self, x_value):
+        denominator = self.p2.evaluate(x_value)
+        if denominator == 0:
+            raise ValueError("division by 0")
+        return self.p1.evaluate(x_value) / denominator
+
 
 poly = Add(Add(Int(4), Int(3)), Add(X(), Mul(Int(1), Add(Mul(X(), X()), Int(1)))))
 print(poly)
+print(poly.evaluate(3))
+
+poly = Add(Add(Mul(Int(2), X()), Sub(Int(8), X())), Div(Int(4), Int(2)))
+print(poly)
+print(poly.evaluate(3))
